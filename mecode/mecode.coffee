f6f4d17c905b3c9f3551fb99d1e1267e0968afe7
Alexander Presber <post@momolog.info>
2012-05-25 12:04:46 +0200

    moved mecode to submodule

--- mecode/mecode.coffee
+++
@@ -1,3 +0,0 @@
-$ ->
-  $('div.mecode input.code').on 'keydown', ->
-    $(@).closest('div.mecode').find('.err').slideUp('fast')
--- mecode/mecode.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-    $(function() {
-        return $('div.mecode input.code').on('keydown', function() {
-            return $(this).closest('div.mecode').find('.err').slideUp('fast');
-        });
-    });
-
-}).call(this);
