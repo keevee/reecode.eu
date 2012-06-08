2fd362b198832993780b4b8c591961a1e3bce904
Azin <aazadi@gmail.com>
2012-06-08 14:00:41 +0200

    readd index.php

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php include("mecode/mecode.php"); ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>reecode your life!</title>
+  <link href="css/style.css" rel="stylesheet" type="text/css" />
+  <? meheader(); ?>
+</head>
+
+<body>
+<table style="width: 100%; height: 100%" cellpadding="0" cellspacing="0" marginwidth="0" marginheight="0" border="0">
+  <tr valign="top">
+    <td width="65%" style="background-image:url(img/bg_left_crpd.jpg); background-repeat:repeat-y;
+ background-position:right;" align="right">
+      <table cellpadding="0" cellspacing="0" border="0">
+        <tr><td height="1100"></td>
+          <td width="40"></td>
+        </tr>
+        <tr><td colspan="2" align="right" style="padding-right: 32px">
+          <a name="mainframe" /><img src="img/reecode_Typ1.png" alt="reecode" /></td>
+        </tr>
+        <tr><td colspan="2" height="120"></td>
+        </tr>
+        <tr><td align="right">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <tr><td><table cellpadding="0" cellspacing="0" border="0">
+              <tr><td width="300" bgcolor="#47595b" valign="middle">
+                <img src="img/1x1_white_anim.gif" width="66%" height="12" /></td>
+              </tr>
+            </table></td>
+            </tr>
+            <tr><td class="textwhite" align="right">
+              reesourcing<br />
+              progress</td>
+            </tr>
+          </table></td>
+          <td></td>
+        </tr>
+
+        <tr><td colspan="2" height="1100"></td>
+        </tr>
+
+        <tr><td align="right">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <tr><td><img src="img/corner_ol.gif" /></td>
+              <td></td>
+              <td><img src="img/corner_or.gif" /></td>
+            </tr>
+            <tr><td></td>
+              <td width="286" height="186"></td>
+              <td></td>
+            </tr>
+            <tr><td><img src="img/corner_ul.gif" /></td>
+              <td></td>
+              <td><img src="img/corner_ur.gif" /></td>
+            </tr>
+          </table></td>
+          <td></td>
+        </tr>
+        <tr><td colspan="2" height="10"></td>
+        </tr>
+        <tr><td align="right">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <tr><td width="300" bgcolor="#47595b" align="left">
+              <img src="img/1x1_white_anim.gif" width="34%" height="12" /></td>
+            </tr>
+          </table></td>
+          <td></td>
+        </tr>
+        <tr><td class="textwhite" align="right">
+          reesourcing<br />
+          progress</td>
+          <td></td>
+        </tr>
+
+      </table></td>
+    <!-- BALKAN -->
+    <td bgcolor="#313f40" style="background-image:url(img/reesourcingstripe01_128-30.gif); background-repeat:repeat-y;" valign="top">
+      <img src="img/1x1_trans.gif" width="100" height="2" /></td>
+    <!-- RIGHT SIDE -->
+    <td width="35%" style="background-image:url(img/bg_right_crpd.jpg); background-repeat:repeat-y;
+ background-position:left;">
+      <table cellpadding="0" cellspacing="0" border="0">
+        <tr><td rowspan="2" width="35"></td>
+          <td height="40"></td>
+        </tr>
+        <tr><td align="left">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <tr><td>
+                <? mecode(); ?>
+            </td>
+            </tr>
+            <tr><td colspan="2" height="1160"></td>
+            </tr>
+            <tr><td>
+                <? meproduct(2); ?>
+            </td>
+            </tr>
+            <tr><td height="5200" valign="bottom" class="warninggrey">© public reelation force <br /><br /></td>
+            </tr>
+          </table></td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+
+
+</body>
+</html>
